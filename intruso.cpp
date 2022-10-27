fde0061ed01ede8eb9afd49e3c6e0c7f78c60ddd
santastico <111901646+santastico@users.noreply.github.com>
2022-10-27 17:59:25 -0300

    Implementação crack_senha no intruso.cpp

--- intruso.cpp
+++ intruso.cpp
@@ -11,7 +11,7 @@
 
 std :: vector <char> a, b, c, d, e;
 int size_all_password = 0, counter = 0;
-std :: string password;
+std :: string password = "         ";
 
 
 void Intruso :: set_senha_vazada(std::string vazou){
@@ -30,6 +30,53 @@ void Intruso :: set_senha_vazada(std::string vazou){
     }
 }
     
+//Implementado, mas apenas vai testar as duas primeiras senhas;
 std::string Intruso ::  crack_senha(){
+    std :: string count;
+    for(int i = 0; i < 10; i++){
+        switch(all_passwords[i]){
+            case 'A':
+                    for(int j = 0; j < 2; j++){
+                        for(int k = 2; k < 4; k++){
+                            if(a[j] == a[k])
+                            count[i] = a[j];
+                        }
+                    }
+            case 'B':
+                    for(int j = 0; j < 2; j++){
+                        for(int k = 2; k < 4; k++){
+                            if(b[j] == b[k])
+                            count[i] = b[j];
+                        }
+                    }
+            case 'C':
+                    for(int j = 0; j < 2; j++){
+                        for(int k = 2; k < 4; k++){
+                            if(c[j] == c[k])
+                            count[i] = c[j];
+                        }
+                    }
+            case 'D':
+                    for(int j = 0; j < 2; j++){
+                        for(int k = 2; k < 4; k++){
+                            if(d[j] == d[k])
+                            count[i] = d[j];
+                        }
+                    }
+            case 'E':
+                    for(int j = 0; j < 2; j++){
+                        for(int k = 2; k < 4; k++){
+                            if(e[j] == e[k])
+                            count[i] = e[j];
+                        }
+                    }
+            default:
+            //não fazer nada
+        }
+    }
+    for(int j = 0, i = 0; j < 10; j+=2, i++){
+            password[j] = count[i];
+            password[j+1] = ' ';
+    }
     return password;
 }
